8fe53a42068a5cfa15591ec12dd0a25ecd50a6d9
Dylan Burden <dylanburden@dylans-macbook-air.local>
2018-07-27 20:11:24 -0230

    Initial Commit

---
+++ Elemental/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Elemental
+//
+//  Created by Dylan Burden on 2018-07-27.
+//  Copyright © 2018 Elemental. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
